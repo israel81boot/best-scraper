1bc039adac13b02a749e42aa8770b6afb92cd627
israel Dembele <demisrael00@gmail.com>
2020-01-27 11:10:19 -0500

    running server

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var  PORT = process.env.PORT || 3000;
+
 var logger = require("morgan");
-var mongoose = require("mongoose"); 
+var port = express ();
 
-var axios = require("axios"); 
-var cheerio = require("cheerio"); 
+var route = express.Router();
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/holderdb"; 
+mongoose.connect(MONGODB_URI, function(err){
+  assert.equal(null,err); 
+  console.log("connected successfully to server"); 
 
-var db = require("./models");
+},{ useNewUrlParser: true }); 
+app.use(express.static(__dirname = "./public"));
+
+ var assert = require('assert');
+
+
 
 var PORT = 3000; 
 
@@ -13,9 +25,13 @@ var app = express();
 
 app = express(); 
 
-app.use(logger("dev"));
-
-app.use(express.urlencoded({ extended: true})); 
+//app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.json()); 
+// Make public a static folder
 app.use(express.static("public"));
+
+
+app.listen(PORT, function(){
+    console.log("PORT running on port" + PORT + "!");
+});
